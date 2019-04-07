14388e189a157ff22f4e4ad5e5ab8c27d2583954
dmitriy <dmitriy.shtele@gmail.com>
2019-04-07 23:29:03 +0400

    1

---
+++ Test/Lab2.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+struct something 
+{
+	char name[20];
+	char *surname;
+	int id;
+	int a;
+	double b;
+	double *p;
+
+public:
+	something() {
+
+	}
+
+
+};
+
+int main()
+{
+	return 0;
+}
